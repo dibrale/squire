fdb8ea892b004e06153889a9f8a8cec8eb7e6179
Alexander Dibrov <108030031+dibrale@users.noreply.github.com>
2023-05-25 00:35:20 -0500

    Custom Search Update
    - Removed ArXiv search, as the cost in execution time and false positives outweigh the benefits
    - Improved custom DuckDuckGo search script
    - Preprocessor to extract keywords from input prior to search, with template and corresponding option added
    - Support for `n_gpu_layers` for the LLM
    
    **TODO:** Code cleanup.
    
    **Note:** The custom DuckDuckGo search code disables safe search

---
+++ ddgtool.py
@@ -0,0 +1,103 @@
+# This code is mostly copied from LangChain's modules, with a few revisions to facilitate better DuckDuckGo replies
+# It is likely to change as the DuckDuckGo API code is improved
+
+from pydantic import Field
+
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForToolRun,
+    CallbackManagerForToolRun,
+)
+from langchain.tools.base import BaseTool
+from duckduckgo_search import DDGS
+
+"""Util that calls DuckDuckGo Search.
+
+No setup required. Free.
+https://pypi.org/project/duckduckgo-search/
+"""
+from typing import Dict, List, Optional
+
+from pydantic import BaseModel, Extra
+from pydantic.class_validators import root_validator
+
+
+class DuckDuckGoCustomAPIWrapper(BaseModel):
+    """Wrapper for DuckDuckGo Search API.
+
+    Free and does not require any setup
+    """
+    k: int = 10
+    region: Optional[str] = "wt-wt"
+    safesearch: str = "Off"
+    time: Optional[str] = "y"
+    max_results: int = 5
+
+    class Config:
+        """Configuration for this pydantic object."""
+
+        extra = Extra.forbid
+
+    @root_validator()
+    def validate_environment(cls, values: Dict) -> Dict:
+        """Validate that python package exists in environment."""
+        try:
+            from duckduckgo_search import ddg  # noqa: F401
+        except ImportError:
+            raise ValueError(
+                "Could not import duckduckgo-search python package. "
+                "Please install it with `pip install duckduckgo-search`."
+            )
+        return values
+
+    def get_snippets(self, query: str) -> List[str]:
+        """Run query through DuckDuckGo and return concatenated results."""
+
+        ddgs = DDGS()
+        ddgs_text_gen = ddgs.text(query, safesearch=self.safesearch)
+
+        snippets = []
+        snippet_number = 0
+        for r in ddgs_text_gen:
+            snippets.append(str(r))
+            # print('Debug print of DDG snippet: ' + str(r))
+            snippet_number += 1
+            if snippet_number > self.max_results:
+                break
+
+        if not snippets:
+            return ["No good DuckDuckGo Search Result was found. This often happens when results are censored."]
+        return snippets
+
+    def run(self, query: str) -> str:
+        snippets = self.get_snippets(query)
+        return "\n".join(snippets)
+
+
+class DuckDuckGoCustomRun(BaseTool):
+    """Custom tool for DuckDuckGo search that does not use safe search"""
+
+    name = "DuckDuckGo Search"
+    description = (
+        "A wrapper around DuckDuckGo Search. "
+        "Useful for when you need to answer questions about current events. "
+        "Input should be a search query."
+    )
+    api_wrapper: DuckDuckGoCustomAPIWrapper = Field(
+        default_factory=DuckDuckGoCustomAPIWrapper
+    )
+
+    def _run(
+        self,
+        query: str,
+        run_manager: Optional[CallbackManagerForToolRun] = None,
+    ) -> str:
+        """Use the tool."""
+        return self.api_wrapper.run(query)
+
+    async def _arun(
+        self,
+        query: str,
+        run_manager: Optional[AsyncCallbackManagerForToolRun] = None,
+    ) -> str:
+        """Use the tool asynchronously."""
+        raise NotImplementedError("DuckDuckGoSearch does not support async")
--- squire.py
+++ squire.py
@@ -14,12 +14,13 @@ from langchain import LLMChain, PromptTemplate
 from langchain.agents import Tool, AgentExecutor, initialize_agent, AgentType
 from langchain.callbacks.base import BaseCallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain.chains import SequentialChain
 from langchain.llms import LlamaCpp
 from langchain.prompts import MessagesPlaceholder
 from langchain.schema import AgentAction
-from langchain.tools import DuckDuckGoSearchRun
-from langchain.utilities import ArxivAPIWrapper, WikipediaAPIWrapper
+from langchain.utilities import WikipediaAPIWrapper
+
+from ddgtool import DuckDuckGoCustomRun
+
 
 # Initialize parameters, along with any values that should not be changed in command line
 params = {
@@ -44,23 +45,28 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('-q', '--question', type=str, default='question.txt',
                     help='path to a *.txt file containing your question')
+parser.add_argument('-w', '--keyword_template', type=str, default='keyword_template.txt',
+                    help='path to template *.txt file')
 parser.add_argument('-m', '--template', type=str, default='template.txt',
                     help='path to template *.txt file')
-parser.add_argument('-l', '--llama_path', type=str, default="ggml-model-q4_0.bin",
+parser.add_argument('-l', '--llama_path', type=str,
+                    default="ggml-model-q4_0.bin",
                     help='path to ggml model weights *.bin file')
 parser.add_argument('-o', '--output', type=str, default='out.txt',
                     help='path to output file for the final answer')
 parser.add_argument('-p', '--top_p', type=float, default=0.7)
 parser.add_argument('-r', '--repeat_penalty', type=float, default=1.1)
-parser.add_argument('-k', '--top_k', type=float, default=30)
+parser.add_argument('-k', '--top_k', type=int, default=30)
 parser.add_argument('-T', '--temperature', type=float, default=0.4)
-parser.add_argument('-b', '--n_batch', type=float, default=512)
-parser.add_argument('-t', '--n_threads', type=float, default=8)
+parser.add_argument('-b', '--n_batch', type=int, default=512)
+parser.add_argument('-g', '--n_gpu_layers', type=int, default=0)
+parser.add_argument('-t', '--n_threads', type=int, default=8)
 parser.add_argument('-v', '--verbose', default=False, action='store_true', help='verbose AgentExecutor output')
 params.update(vars(parser.parse_args()))
 
 # Make extra sure that the most important parameters are not empty
 assert params['template']
+assert params['keyword_template']
 assert params['question']
 assert params['llama_path']
 assert params['output']
@@ -71,6 +77,14 @@ if params['template'][-4:] != '.txt':
 elif os.path.isfile(params['template']):
     with open(params['template'], 'r') as f:
         params.update({'template_text': '\n'.join(f.readlines())})
+else:
+    raise FileNotFoundError(params['template'])
+
+if params['keyword_template'][-4:] != '.txt':
+    raise ValueError('Supply a keyword extraction template in a *.txt file with the \'-w\' option.')
+elif os.path.isfile(params['keyword_template']):
+    with open(params['keyword_template'], 'r') as f:
+        params.update({'keyword_template_text': '\n'.join(f.readlines())})
 else:
     raise FileNotFoundError(params['template'])
 
@@ -89,19 +103,21 @@ elif not os.path.isfile(params['llama_path']):
 
 
 # 'self ask with search' agent chain initialization function
-def chain_init(tool_wrapper, language_model, verbose=True) -> langchain.agents.initialize:
-
+def chain_init(
+        tool_wrapper, language_model, verbose=True, description="useful for when you need to ask with search"
+) -> langchain.agents.initialize:
     # Function to make tools for 'self ask with search'-type agents
     def make_intermediate(wrapper) -> list[Tool]:
         return [Tool(name="Intermediate Answer", func=wrapper.run,
-                     description="useful for when you need to ask with search")]
+                     description=description)]
 
     return initialize_agent(
         tools=make_intermediate(tool_wrapper),
         llm=language_model,
         callback_manager=callback_manager,
         agent=AgentType.SELF_ASK_WITH_SEARCH,
-        verbose=verbose)
+        verbose=verbose
+    )
 
 
 # Wrapper for agents that allows them to retry in case of parse errors
@@ -126,6 +142,8 @@ def agent_wrapper(executor: AgentExecutor, question: str, iterations: int = 5, r
             print('\n', e)
             exiting = True
             tries_left = 0
+        except ValueError as e:
+            print('\n', e)
         finally:
             if not exiting:
                 tries_left -= 1
@@ -146,37 +164,43 @@ llm = LlamaCpp(
     temperature=params['temperature'],
     n_batch=params['n_batch'],
     n_threads=params['n_threads'],
+    n_gpu_layers=params['n_gpu_layers'],
     echo=True,
     # stop=['Human:', 'Result:', 'Observation:']
 )
 
+# Prepare keyword prompt and initialize keyword extraction chain
+keyword_prompt = PromptTemplate(input_variables=['question'], template=params['keyword_template_text'])
+keyword_chain = LLMChain(
+    llm=llm,
+    prompt=keyword_prompt,
+    verbose=params['verbose'],
+    output_key='keywords')
+
 # Prepare summary prompt and initialize summary chain
-summary_prompt = PromptTemplate(input_variables=['search', 'wiki', 'arxiv'], template=params['template_text'])
+summary_prompt = PromptTemplate(input_variables=['question', 'search', 'wiki'], template=params['template_text'])
 summary_chain = LLMChain(
     llm=llm,
     prompt=summary_prompt,
     verbose=params['verbose'],
     output_key='summary')
 
-# Initialize search agent chains
-search = agent_wrapper(chain_init(DuckDuckGoSearchRun(), llm, params['verbose']), params['question_text'])
-wiki = agent_wrapper(chain_init(WikipediaAPIWrapper(), llm, params['verbose']), params['question_text'])
-arxiv = agent_wrapper(chain_init(ArxivAPIWrapper(), llm, params['verbose']), params['question_text'])
+# Initialize keyword extraction chain
+keywords = keyword_chain.run({'question': params['question_text']})
 
-'''
-# Initialize overall chain
-overall_chain = SequentialChain(
-    chains=[summary_chain],
-    input_variables=["search", "wiki", "arxiv"],
-    output_variables=['summary'],
-    verbose=params['verbose'])
-'''
+# Prepare keyword string
+keyword_str = f"Find information about these keywords: {keywords}"
+
+# Initialize search agent chains
+search = agent_wrapper(chain_init(DuckDuckGoCustomRun(), llm, params['verbose']), keyword_str, iterations=3)
+wiki = agent_wrapper(chain_init(WikipediaAPIWrapper(), llm, params['verbose'], "useful for entries from Wikipedia"),
+                     keyword_str, iterations=3)
 
 # Check for no answer condition and output accordingly, or run summary if answer is present
-if search == wiki == arxiv == no_data_string:
+if search == wiki == no_data_string:
     output = 'Unable to answer the question.'
 else:
-    output = summary_chain.run({'search': search, 'wiki': wiki, 'arxiv': arxiv})
+    output = summary_chain.run({'question': params['question_text'], 'search': search, 'wiki': wiki})
 
 # Write the output
 with open(params['output'], 'w') as f:
